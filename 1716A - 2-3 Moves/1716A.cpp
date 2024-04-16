d03cbc5c6c6466d0f10bbdd8335604a5781f1464
AvisCodeX <avishekchandradas@gmail.com>
2024-04-17 01:39:19 +0600

    1716A - 2-3 Moves

---
+++ 1716A - 2-3 Moves/1716A.cpp
@@ -0,0 +1,19 @@
+//AvisCodeX
+
+#include<bits/stdc++.h>
+using namespace std ;
+
+int main()
+{
+    int t ;
+    cin >> t ;
+    while( t-- )
+    {
+        int n ; 
+        cin >> n ;
+        if( n == 1 ) cout << "2" << endl ;
+        else if( n % 3 != 0 ) cout << ( n / 3 ) + 1 << endl ;
+        else if( n % 3 == 0 ) cout << n / 3 << endl ;
+    }
+    return 0;
+}
