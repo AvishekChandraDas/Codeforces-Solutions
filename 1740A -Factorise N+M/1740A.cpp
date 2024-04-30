2d261fb61662e148aabd9f964a4651d16785721a
AvisCodeX <avishekchandradas@gmail.com>
2024-04-30 22:43:35 +0600

    1740A - Factorise N+M

---
+++ 1740A -Factorise N+M/1740A.cpp
@@ -0,0 +1,25 @@
+//AvisCodeX
+
+#include<bits/stdc++.h>
+using namespace std ;
+
+int main()
+{
+    long int t ;
+    cin >> t ;
+
+    while( t-- )
+    {
+        long int n ;
+        cin >> n ;
+        if(n%2)
+        {
+            cout << "3" << endl ;
+        }
+        else
+        {
+            cout << "2" << endl ; 
+        }
+    }
+    
+}
